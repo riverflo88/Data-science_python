008c116716a09b07e668fb3d35727bc367e6a8c9
Riverflo <78293875+riverflo88@users.noreply.github.com>
2023-09-28 13:57:15 +0100

    Add files via upload

---
+++ Control_Structures-For_loop/README.md
@@ -0,0 +1,9 @@
+task3.py.
+
+● Create a new Python file in this folder called task3.py.
+● Create a loop that will produce the following output:
+*
+**
+***
+****
+*****
---
+++ Control_Structures-For_loop/task3 (2).py
@@ -0,0 +1,5 @@
+﻿#creates a for loop that loops over a range of numbers from 1-5
+for i in range(1,6):
+     # creates i number of "*"
+     symbol = i * ("*")
+     print(symbol)
